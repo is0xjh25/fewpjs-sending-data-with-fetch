3a57e6961bbc143fa2a4fe95b86d4a6cd86cca8a
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-13 23:18:42 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(user, address) {
+
+	const form = {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			name: user,
+			email: address,
+		})
+	}
+
+	fetch("http://localhost:3000/users", form)
+	.then(function(response) {
+		return response.json();
+	})
+	.then((object) => {
+		document.body.innerHTML = object.id;
+	})
+	.catch((error) => {
+		document.body.innerHTML = error.message;
+	});  
+}
+  
